1de6b00038f6e6216f9e530c2bd6f4b7d1e0a8e0
Daniel França Almeida <daniel.franca0512@gmail.com>
2024-04-13 00:56:05 -0300

    Adição do contexto de banco de dados SQLite com Migrations

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,61 +13,25 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-          
-        public IEnumerable<Evento> _evento = new Evento[]{
-                new Evento{
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "São Paulo",
-                Lote = "1º Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemUrl = "foto.png"
-                },
-                new Evento{
-                EventoId = 2,
-                Tema = "Angular 11 e .NET 6",
-                Local = "Rio Grande Do Sul",
-                Lote = "2º Lote",
-                QtdPessoas = 50,
-                DataEvento = DateTime.Now.AddDays(3).ToString(),
-                ImagemUrl = "foto2.png"
-                },
-                new Evento{
-                EventoId = 3,
-                Tema = "Angular 11 e .NET 7",
-                Local = "São Luiz",
-                Lote = "1º Lote",
-                QtdPessoas = 350,
-                DataEvento = DateTime.Now.AddDays(4).ToString(),
-                ImagemUrl = "foto3.png"
-                },
-                new Evento{
-                EventoId = 4,
-                Tema = "Angular 11 e .NET 8",
-                Local = "Rio Grande Do Norte",
-                Lote = "4º Lote",
-                QtdPessoas = 100,
-                DataEvento = DateTime.Now.AddDays(5).ToString(),
-                ImagemUrl = "foto5.png"
-                }
-            };
+              
+        private readonly DataContext _context;
     
-        public EventoController()
+        public EventoController(DataContext context)
         {
-           
+            this._context = context;
+            
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id){
+        public Evento GetById(int id){
             
-            return _evento.Where(e => e.EventoId == id);
+            return _context.Eventos.FirstOrDefault(e => e.EventoId == id);
         }
 
  
